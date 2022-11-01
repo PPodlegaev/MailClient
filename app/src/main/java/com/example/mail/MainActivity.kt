41a967d25051dc14a2c129bfcdacc06abc9b97e5
ppodl <ppodlegaev@gmail.com>
2022-11-01 10:08:01 +0300

    ver 2.0

--- app/src/main/java/com/example/mail/MainActivity.kt
+++ app/src/main/java/com/example/mail/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.mail
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mail.databinding.ActivityMainBinding
 import android.view.View
@@ -12,55 +11,46 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private val pickFromGallery:Int = 101
-    lateinit var uri: Uri
+    private lateinit var uri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Переход на страницу настроек
         binding.PageSettings.setOnClickListener{
             val intent = Intent(this, SettingsPage::class.java)
             startActivity(intent)
 
         }
 
-        //Закрепить Файл
+        //Выбор картинки
         binding.btAttachment.setOnClickListener{
             openFolder()
         }
 
-
-
-
+        //Переход в почтовый клиент и закрепление сообщения и вложения
         binding.sendBtn.setOnClickListener{
             val email = binding.mailAddress.text.toString()
             val subject = binding.Subject.text.toString()
             val message = binding.Message.text.toString()
 
             val address = email.split(",".toRegex()).toTypedArray()
-            val intent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:")
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
                 putExtra(Intent.EXTRA_EMAIL,address)
                 putExtra(Intent.EXTRA_SUBJECT, subject)
                 putExtra(Intent.EXTRA_TEXT, message)
+                putExtra(Intent.EXTRA_STREAM, uri)
             }
-            if (uri != null) {
-                intent.putExtra(Intent.EXTRA_STREAM, uri);
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
             }
-
-            try {
-                startActivity(Intent.createChooser(intent, "Choose Email Client"))
-            } catch (e: Exception) {
-                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-            }
-
         }
-
-
     }
 
-    fun openFolder() {
+    private fun openFolder() {
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
--- app/src/main/java/com/example/mail/SettingsPage.kt
+++ app/src/main/java/com/example/mail/SettingsPage.kt
@@ -1,47 +1,45 @@
 package com.example.mail
 
-
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CalendarView
-import android.widget.DatePicker
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mail.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.mail.databinding.ActivitySettingsPageBinding
 import java.util.*
 
 
 class SettingsPage : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySettingsPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings_page)
+        binding = ActivitySettingsPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val _btn_Theme = findViewById<Button>(R.id.btn_Theme)
-        _btn_Theme.setOnClickListener { chooseThemeDialog() }
+        binding.btnTheme.setOnClickListener { chooseThemeDialog() } //Выбор темы
 
+        binding.btnBack.setOnClickListener { finish() } //Кнопка назад
 
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        val Date = findViewById<Button>(R.id.PickDate)
-        val _textDate = findViewById<TextView>(R.id.textDate)
-        Date.setOnClickListener{
+        //Календарь
+        binding.PickDate.setOnClickListener{
             val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, mYear, mMonth,mDay ->
-                _textDate.setText(""+ mDay +"/"+ mMonth +"/"+ mYear)
+                binding.textDate.setText(""+ mDay +"/"+ mMonth +"/"+ mYear)
 
             }, year, month, day)
             dpd.show()
 
         }
+
     }
 
+    //Тема
     private fun chooseThemeDialog() {
 
         val builder = AlertDialog.Builder(this)
@@ -53,19 +51,19 @@ class SettingsPage : AppCompatActivity() {
 
             when (which) {
                 0 -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //Включение темной темы
 
                     delegate.applyDayNight()
                     dialog.dismiss()
                 }
                 1 -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //Включение светлой темы
                     delegate.applyDayNight()
 
                     dialog.dismiss()
                 }
                 2 -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) //Включение системной темы
                     delegate.applyDayNight()
                     dialog.dismiss()
                 }
@@ -76,6 +74,5 @@ class SettingsPage : AppCompatActivity() {
         val dialog = builder.create()
         dialog.show()
     }
-
 
 }
